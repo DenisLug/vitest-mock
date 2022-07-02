ce501d138e7c807fddeeea5121fc7a21fc6b577e
Denis Lugowski <denis.lugowski@gmail.com>
2022-07-02 08:21:06 +0200

    Some cleanup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,23 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true,
-        "vue/setup-compiler-macros": true,
-    },
-    plugins: ["unused-imports"],
-    rules: {
-        "comma-dangle": ["error", "never"],
-        "no-console": "off",
-        "object-curly-spacing": ["error", "always"],
-        "vue/multi-word-component-names": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "unused-imports/no-unused-imports": "warn",
-    },
-    extends: [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential",
-        "@vue/eslint-config-prettier",
-        "@vue/typescript/recommended",
-        "@vue/prettier",
-        "plugin:cypress/recommended",
-    ],
-    ignorePatterns: ["cypress/plugins", "generated", "dist"],
-};
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    'vue/setup-compiler-macros': true
+  },
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'no-console': 'off',
+    'object-curly-spacing': ['error', 'always'],
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-prettier',
+    '@vue/typescript/recommended',
+    '@vue/prettier'
+  ],
+  ignorePatterns: ['dist']
+}
